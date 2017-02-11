7b88e0980f1127f1f6aef66ad564ee17c20fda54
beko99 <beko_18@vps30.com>
2017-02-11 16:46:27 +0300

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -1,10 +1,10 @@
---@d_1_t
+--@deve_poent
 --@DEV_s_o
 local function run(msg, matches)
-if matches[1] == 'ايدي'  then
+if matches[1] == 'id'  then
 tg.sendMessage(msg.chat_id, msg.id_, 1, 'your id : ['..msg.from_id..']️', 1)
 end
-if matches[1] == 'احذف' and is_momod(msg) then
+if matches[1] == 'del' and is_momod(msg) then
    tg.deleteMessages(msg.chat_id_, {[0] = msg.reply_to_message_id_})
 end
 if matches[1] == 'بوت'  then
@@ -13,10 +13,10 @@ end
 if matches[1] == 'المطور'  then
    tg.sendMessage(msg.chat_id, msg.id_, 1, '@d_1_t', 1)
 end
-	if matches[1] == 'ثبت' and is_owner(msg) then
+	if matches[1] == 'pin' and is_owner(msg) then
 	    tg.pinChannelMessage(msg.chat_id_, msg.reply_to_message_id_, 1)
 		tg.sendMessage(msg.chat_id_, 0, 0,  'تم تثبيت الرساله☑️', 0)
-	elseif matches[1] == 'احذف تثبيت' and is_owner(msg) then
+	elseif matches[1] == 'unpin' and is_owner(msg) then
 	    tg.unpinChannelMessage(msg.chat_id_)
 		tg.sendMessage(msg.chat_id_, 0, 0,  ' تم ازالة الرسائل المثبته❌', 0)
 
@@ -24,16 +24,16 @@ end
 end
 return {
   patterns = {
-    "^[#!/](ايدي)$",
+    "^[#!/](id)$",
      "^(بوت)$",
-    "^[#!/](ثبت)$",
+    "^[#!/](pin)$",
     "^(المطور)$",
-    "^[#!/](احذف)$",
-    "^[#!/](احذف تثبيت)$"
+    "^[#!/](del)$",
+    "^[#!/](unpin)$"
      
      
   },
   run = run
 }
---@d_1_t
+--@deve_poent
 --@DEV_s_o
